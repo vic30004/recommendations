213ccf7df6cc3378ea57d291edfb81837b5e93b9
Victor <victor_993@msn.com>
2021-04-07 14:15:43 +0300

    removed unused imports

--- client/src/pages/Homepage/Homepage.js
+++ client/src/pages/Homepage/Homepage.js
@@ -1,9 +1,6 @@
-import React, { useState, useEffect } from "react";
 import { Main } from "../../styles/Homepage";
 import { BorderContainer } from "../../components/Homepage";
-import { Message } from "../../common";
-import useForm from "../../hooks/UseForm";
-import useToggle from "../../hooks/useToggle";
+
 const Homepage = () => {
   return (
     <Main>
