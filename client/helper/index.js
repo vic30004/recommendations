bc3686368ac90d9dabc42ebd9be271063cb24cc5
Victor <victor_993@msn.com>
2021-05-21 17:58:43 +0300

    fixed some errors

--- client/helper/index.js
+++
@@ -1,8 +0,0 @@
-import { useQuery, useMutation } from "@apollo/client";
-import { GET_USER } from "../src/graphql";
-
-// export const getUserByUsername = (username) => {
-//   return ({ data, loading, error } = useQuery(GET_USER, {
-//     variables: { username },
-//   }));
-// };
