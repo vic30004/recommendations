6c89657e4b5351a7b12b59115ee72d9b9df3fa9a
Victor <victor_993@msn.com>
2021-05-21 12:38:51 +0300

    pushing to aws

--- api/knexfile.js
+++ api/knexfile.js
@@ -38,7 +38,13 @@ module.exports = {
 
   production: {
     client: "pg",
-    connection: process.env.DATABASE_URL,
+    connection: {
+      host: process.env.DB_HOST,
+      port: "",
+      user: process.env.DB_USER,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB,
+    },
     migrations: {
       directory: "./db/migrations",
     },
