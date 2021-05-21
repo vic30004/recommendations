6cb8020c687337410a6340dcbd4acc2d04afe874
Victor <victor_993@msn.com>
2021-05-21 19:17:31 +0300

    removed console log

--- api/db/knex.js
+++ api/db/knex.js
@@ -1,6 +1,5 @@
 const environment = process.env.NODE_ENV || "development"; // if something else isn't setting ENV, use development
 
 const knexfile = require("../knexfile")[environment];
-console.log(knexfile);
 const knex = require("knex")(knexfile);
 module.exports = knex;
